28335877daa398ba4cfdccd22f4f21e7107f930f
alilihoc <hocine.alili@gmail.com>
2019-10-11 11:09:25 +0200

    User

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,15 +15,50 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('firstname', TextType::class,[
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('lastname', TextType::class,[
+                'label' => 'Prenom',
+                'attr' => [
+                    'placeholder' => 'Prenom'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'E-mail',
+                'attr' => [
+                    'placeholder' => 'Email'
+                ]
+            ])
             ->add('password', PasswordType::class)
-            ->add('adress')
-            ->add('city')
-            ->add('country')
-            ->add('postalCode')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('phone')
+            ->add('adress', TextType::class,[
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Adresse'
+                ]
+            ])
+            ->add('city', TextType::class,[
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Ville'
+                ]
+            ])
+            ->add('postalCode', TextType::class,[
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Code postal'
+                ]
+            ])
+
+            ->add('phone', TextType::class,[
+                'label' => 'Numéro de telephone',
+                'attr' => [
+                    'placeholder' => 'Numéro de téléphone'
+                ]
+            ])
         ;
     }
 
